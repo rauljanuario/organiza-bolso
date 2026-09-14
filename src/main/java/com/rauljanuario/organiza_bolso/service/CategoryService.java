311d23b042ead145c3b683d88a609548cdd437e4
Raul Januario <raul.s.januario@gmail.com>
2026-09-14 19:47:11 -0300

    feat(service): add categoryservice and createcategory method

---
+++ src/main/java/com/rauljanuario/organiza_bolso/service/CategoryService.java
@@ -0,0 +1,41 @@
+package com.rauljanuario.organiza_bolso.service;
+
+import com.rauljanuario.organiza_bolso.dto.category_dto.GetCategoryDTO;
+import com.rauljanuario.organiza_bolso.dto.category_dto.PostCategoryDTO;
+import com.rauljanuario.organiza_bolso.model.Category;
+import com.rauljanuario.organiza_bolso.model.User;
+import com.rauljanuario.organiza_bolso.repository.CategoryRepository;
+import com.rauljanuario.organiza_bolso.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
+
+    public CategoryService(UserRepository userRepository, CategoryRepository categoryRepository) {
+        this.userRepository = userRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    public ResponseEntity<GetCategoryDTO> saveCategory(PostCategoryDTO data) {
+
+        User user = userRepository.findById(data.id())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        var category = new Category();
+        category.setUser(user);
+        category.setName(data.name());
+        category.setType(data.type());
+        category.setCreatedAt(java.time.LocalDateTime.now());
+
+        Category saved = categoryRepository.save(category);
+
+        return ResponseEntity.ok(new GetCategoryDTO(saved.getId(), saved.getName(), saved.getType()));
+    }
+
+
+}
