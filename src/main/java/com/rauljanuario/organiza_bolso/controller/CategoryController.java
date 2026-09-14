12ca097231ede881c1fb8e38b0b3ebcf2bfb7fe3
Raul Januario <raul.s.januario@gmail.com>
2026-09-14 19:45:42 -0300

    feat(controller): add categorycontroller constructor

--- src/main/java/com/rauljanuario/organiza_bolso/controller/CategoryController.java
+++ src/main/java/com/rauljanuario/organiza_bolso/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.rauljanuario.organiza_bolso.controller;
 
 import com.rauljanuario.organiza_bolso.dto.category_dto.GetCategoryDTO;
 import com.rauljanuario.organiza_bolso.dto.category_dto.PostCategoryDTO;
+import com.rauljanuario.organiza_bolso.service.CategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,10 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/categories")
 public class CategoryController {
 
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @PostMapping
     public ResponseEntity<GetCategoryDTO> createCategory(@RequestBody PostCategoryDTO data) {
 
-        GetCategoryDTO result = controllerService.createCategory(data).getBody();
+        GetCategoryDTO result = categoryService.saveCategory(data).getBody();
 
         return ResponseEntity.ok(result);
 
